193803fe47007bb7bb0d27f0dee7d57a7ae5ae6d
qzart45hi <qwert45hi@yandex.ru>
2022-09-05 23:09:30 +0300

    feat: borrow the main code part

--- server.ts
+++ server.ts
@@ -1,6 +1,107 @@
-import { Server }  from "socket.io";
+import { version, validate } from "uuid";
+import { Server } from "socket.io";
 
 const io = new Server({});
 const PORT = parseInt(process.env.PORT) || 5000;
+
+// adapted & modified from: https://github.com/maks1mp/video-chat-webrtc
+
+const ACTIONS = {
+  JOIN: "join",
+  LEAVE: "leave",
+  SHARE_ROOMS: "share-rooms",
+  ADD_PEER: "add-peer",
+  REMOVE_PEER: "remove-peer",
+  RELAY_SDP: "relay-sdp",
+  RELAY_ICE: "relay-ice",
+  ICE_CANDIDATE: "ice-candidate",
+  SESSION_DESCRIPTION: "session-description"
+};
+
+function getClientRooms() {
+  const { rooms } = io.sockets.adapter;
+
+  return Array.from(rooms.keys()).filter(roomID => validate(roomID) && version(roomID) === 4);
+}
+
+function shareRoomsInfo() {
+  io.emit(ACTIONS.SHARE_ROOMS, {
+    rooms: getClientRooms()
+  })
+}
+
+io.on("connection", socket => {
+  shareRoomsInfo();
+
+  socket.on(ACTIONS.JOIN, config => {
+    const { room: roomID } = config;
+    const { rooms: joinedRooms } = socket;
+
+    if (Array.from(joinedRooms).includes(roomID)) {
+      return `Already joined to ${roomID}`;
+    }
+
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+    clients.forEach(clientID => {
+      io.to(clientID).emit(ACTIONS.ADD_PEER, {
+        peerID: socket.id,
+        createOffer: false
+      });
+
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerID: clientID,
+        createOffer: true,
+      });
+    });
+
+    socket.join(roomID);
+    shareRoomsInfo();
+  });
+
+  function leaveRoom() {
+    const { rooms } = socket;
+
+    Array.from(rooms)
+      .filter(roomID => validate(roomID) && version(roomID) === 4)
+      .forEach(roomID => {
+
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+        clients
+          .forEach(clientID => {
+            io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+              peerID: socket.id,
+            });
+
+            socket.emit(ACTIONS.REMOVE_PEER, {
+              peerID: clientID,
+            });
+          });
+
+        socket.leave(roomID);
+      });
+
+    shareRoomsInfo();
+  }
+
+  socket.on(ACTIONS.LEAVE, leaveRoom);
+  socket.on("disconnecting", leaveRoom);
+
+  socket.on(ACTIONS.RELAY_SDP, ({ peerID, sessionDescription }) => {
+    io.to(peerID).emit(ACTIONS.SESSION_DESCRIPTION, {
+      peerID: socket.id,
+      sessionDescription,
+    });
+  });
+
+  socket.on(ACTIONS.RELAY_ICE, ({ peerID, iceCandidate }) => {
+    io.to(peerID).emit(ACTIONS.ICE_CANDIDATE, {
+      peerID: socket.id,
+      iceCandidate,
+    });
+  });
+
+});
 
 io.listen(PORT);
